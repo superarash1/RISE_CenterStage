1229a476be847711cd2325cbb9a7116706fd17af
superarash1 <avs.arash@gmail.com>
2024-03-10 22:34:49 -0400

    Fixing Arm code errors

--- Bartholomew/src/main/java/org/firstinspires/ftc/team8109_Rise/Mechanisms/Bartholomew_Arm.java
+++ Bartholomew/src/main/java/org/firstinspires/ftc/team8109_Rise/Mechanisms/Bartholomew_Arm.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcontroller.Hardware.Arms.ServoArm;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.team8109_Rise.Robots.SlidesBot.Mechanisms.ViperSlides;
 
 public class Bartholomew_Arm extends ServoArm {
     static String[] name = {"armLeft", "armRight"};
@@ -48,99 +47,12 @@ public class Bartholomew_Arm extends ServoArm {
             case OUTTAKE_POSITION:
                 setAngle(257);
                 break;
-            case DUNK_POSITION:
-                setAngle(264);
-                break;
-            case AUTO_START:
-                setAngle(252);
-                break;
-            case AUTO_NEW:
-                setAngle(90);
-                break;
             case MANUAL:
                 if (gamepad1.right_bumper){
                     setAngle(getPositionDegrees()+1);
                 } else if (gamepad1.left_bumper){
                     setAngle(getPositionDegrees()-1);
                 }
-                break;
-        }
-    }
-    public void togglePosition(){
-        setArmPosition();
-        switch (servoPosition){
-            case INTAKE_POSITION:
-                if ((gamepad1.x != lastToggleX) && gamepad1.x && toggle1){
-                    toggle1 = false;
-                    toggle2 = true;
-
-                    servoPosition = ServoPosition.OUTTAKE_POSITION;
-                }
-
-                break;
-
-            case OUTTAKE_POSITION:
-                if ((gamepad1.x != lastToggleX) && gamepad1.x && toggle2){
-                    toggle2 = false;
-                    toggle1 = true;
-
-                    servoPosition = ServoPosition.INTAKE_POSITION;
-                }
-
-                if ((gamepad1.left_bumper != lastTriggerLeft) && gamepad1.left_bumper && triggerToggle1){
-                    triggerToggle1 = false;
-                    triggerToggle2 = true;
-
-                    servoPosition = ServoPosition.DUNK_POSITION;
-                }
-                break;
-            case DUNK_POSITION:
-                if ((gamepad1.x != lastToggleX) && gamepad1.x && toggle2){
-                    toggle2 = false;
-                    toggle1 = true;
-
-                    triggerToggle1 = false;
-                    triggerToggle2 = true;
-                    servoPosition = ServoPosition.INTAKE_POSITION;
-                }
-
-                if ((gamepad1.left_bumper != lastTriggerLeft) && gamepad1.left_bumper && triggerToggle2){
-                    triggerToggle2 = false;
-                    triggerToggle1 = true;
-                    servoPosition = ServoPosition.OUTTAKE_POSITION;
-                }
-                break;
-        }
-        lastToggleX = gamepad1.x;
-        lastTriggerLeft = gamepad1.left_bumper;
-    }
-
-    public void slidesToggle(ViperSlides.SlidesState slidesState){
-        setArmPosition();
-        switch (slidesState){
-            case GROUND:
-                servoPosition = ServoPosition.INTAKE_POSITION;
-                break;
-
-            case LOW_JUNCTION:
-                servoPosition = ServoPosition.OUTTAKE_POSITION;
-                break;
-            case MIDDLE_JUNCTION:
-                servoPosition = ServoPosition.OUTTAKE_POSITION;
-                break;
-
-            case HIGH_JUNCTION:
-                servoPosition = ServoPosition.OUTTAKE_POSITION;
-                break;
-
-            case LOW_DUNK:
-                servoPosition = ServoPosition.DUNK_POSITION;
-                break;
-            case MIDDLE_DUNK:
-                servoPosition = ServoPosition.DUNK_POSITION;
-                break;
-            case HIGH_DUNK:
-                servoPosition = ServoPosition.DUNK_POSITION;
                 break;
         }
     }
